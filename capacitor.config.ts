53ccc934f094a9046879469276dbdfb7c786e4ab
Shantelle Wright <wrightshantelle@gmail.com>
2026-02-08 05:00:04 +0000

    chore(ci): add iOS build workflow

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.shantelle.hoodlingo',
+  appName: 'Hoodlingo',
+  webDir: 'dist'
+};
+
+export default config;
